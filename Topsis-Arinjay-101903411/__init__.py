dd99a4c94a1807afb1854e468b39e6566af62438
Arinjay Shukla <56226548+Arinjay24@users.noreply.github.com>
2022-02-27 01:08:41 +0530

    Add files via upload

---
+++ Topsis-Arinjay-101903411/__init__.py
@@ -0,0 +1,5 @@
+import Topsis-Arinjay-101903411.101903411 import main
+import Topsis-Arinjay-101903411.101903411 import calculate_topsis
+import Topsis-Arinjay-101903411.101903411 import topsis_cal
+
+
