00c1e6c9e80c369f3bf16fa60f8730c2893575b8
Arinjay Shukla <56226548+Arinjay24@users.noreply.github.com>
2022-02-27 01:25:41 +0530

    Update __init__.py

--- Topsis-Arinjay-101903411/__init__.py
+++ Topsis-Arinjay-101903411/__init__.py
@@ -1,5 +1,3 @@
-import Topsis-Arinjay-101903411.101903411 import main
-import Topsis-Arinjay-101903411.101903411 import calculate_topsis
-import Topsis-Arinjay-101903411.101903411 import topsis_cal
+
 
 
