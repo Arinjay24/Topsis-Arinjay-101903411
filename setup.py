34fc401e89f311cbe3ac0f8beca0f35933587213
Arinjay Shukla <56226548+Arinjay24@users.noreply.github.com>
2022-02-27 20:13:16 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Arinjay Shukla',                   # Type in your name
   author_email = 'ashukla1_be19@thapar.edu',      # Type in your E-Mail
   url = 'https://github.com/Arinjay24/Topsis-Arinjay-101903411',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Arinjay24/Topsis-Arinjay-101903411/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Arinjay24/Topsis-Arinjay-101903411/archive/refs/tags/v_01.tar.gz',    # I explain this later on
   keywords = ['Easy to Use', 'Fascinating concept'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
